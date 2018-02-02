61ef338e638d3df1fe3a1c9be7493fe8bd39a442
Jeff Knox <knoxjeffrey@outlook.com>
2018-02-02 12:57:12 +0000

    further alts to migrations

--- db/migrate/20171124103352_change_slickr_pages_page_id_attribute.rb
+++ db/migrate/20171124103352_change_slickr_pages_page_id_attribute.rb
@@ -1,5 +1,7 @@
-class ChangeSlickrPagesPageIdAttribute < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :slickr_pages, :page_id, :slickr_page_id
+if ActiveRecord::Base.connection.tables.exclude?('slickr_pages')
+  class ChangeSlickrPagesPageIdAttribute < ActiveRecord::Migration[5.1]
+    def change
+      rename_column :slickr_pages, :page_id, :slickr_page_id
+    end
   end
 end
--- db/migrate/20171124113241_create_slickr_images.rb
+++ db/migrate/20171124113241_create_slickr_images.rb
@@ -1,11 +1,13 @@
-class CreateSlickrImages < ActiveRecord::Migration[5.1]
-  def change
-    create_table :slickr_images do |t|
-      t.string  :attachment
-      t.jsonb   :dimensions
-      t.jsonb   :data, default: {"alt_text": ""}
+if ActiveRecord::Base.connection.tables.exclude?('slickr_images')
+  class CreateSlickrImages < ActiveRecord::Migration[5.1]
+    def change
+      create_table :slickr_images do |t|
+        t.string  :attachment
+        t.jsonb   :dimensions
+        t.jsonb   :data, default: {"alt_text": ""}
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end

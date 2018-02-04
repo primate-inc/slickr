80b3ba306f9eee2e0d7142e1a4f833d7d641c9fa
Jeff Knox <knoxjeffrey@outlook.com>
2018-02-04 00:56:11 +0000

    allow engine uploader storage to be defined in main app carrierwave config

--- app/uploaders/slickr_image_uploader.rb
+++ app/uploaders/slickr_image_uploader.rb
@@ -2,13 +2,13 @@ class SlickrImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.env}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   process :store_dimensions
--- lib/slickr/version.rb
+++ lib/slickr/version.rb
@@ -1,3 +1,3 @@
 module Slickr
-  VERSION = '0.1.19'
+  VERSION = '0.2.0'
 end

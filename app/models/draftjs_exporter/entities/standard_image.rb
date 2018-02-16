76c5398409a7a72c845e61030eda233ae7d55ce9
Jeff Knox <knoxjeffrey@outlook.com>
2018-02-16 12:37:23 +0000

    prevent div wrappers around video and image

--- app/models/draftjs_exporter/entities/standard_image.rb
+++ app/models/draftjs_exporter/entities/standard_image.rb
@@ -3,7 +3,7 @@ class DraftjsExporter::Entities::StandardImage
      size = data[:data][:display].to_sym
      args = { src: data[:data][:image][:attachment][size][:url] }
      element = parent_element.document.create_element('img', args)
-     parent_element.add_child(element)
+     parent_element.replace(element)
      element
   end
 end
--- app/models/draftjs_exporter/entities/vimeo.rb
+++ app/models/draftjs_exporter/entities/vimeo.rb
@@ -2,8 +2,7 @@ class DraftjsExporter::Entities::Vimeo
   def call(parent_element, data)
     args = data[:data][:iframe]
     template = DraftjsExporter::Generators::VimeoGenerator.new(args).render
-
-    parent_element.add_child(template)
+    parent_element.replace(template)
     parent_element
   end
 end

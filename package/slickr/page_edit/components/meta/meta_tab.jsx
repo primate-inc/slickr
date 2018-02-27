d8f40f92864fcee936009801ce904c42501b1975
Bart Oleszczyk <bart@primate.co.uk>
2018-02-27 22:06:23 +0000

    updated seo tab

--- package/slickr/page_edit/components/meta/meta_tab.jsx
+++ package/slickr/page_edit/components/meta/meta_tab.jsx
@@ -5,9 +5,6 @@ export default class MetaTab extends React.Component {
   render() {
     const handleChange = this.props.handleChange
     const values = this.props.values
-    const layoutOptions = this.props.pageLayouts.map(function(layout, index){
-      return <option key={index} value={layout.value}>{layout.display_name}</option>
-    })
     return (
       <fieldset>
         <ol>

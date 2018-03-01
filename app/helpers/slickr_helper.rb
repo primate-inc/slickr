c7728352f8b3f52b4888135955ee047a04ae4bed
Jeff Knox <knoxjeffrey@outlook.com>
2018-03-01 00:28:45 +0000

    time based greeting fix

--- app/helpers/slickr_helper.rb
+++ app/helpers/slickr_helper.rb
@@ -13,6 +13,12 @@ module SlickrHelper
     form_json(page)
   end
 
+  def time_of_day_greeting(name)
+    time_greeting = greeting_wording
+    ending = name.nil? ? '!' : ", #{name}!"
+    time_greeting + ending
+  end
+
   private
 
   def restructure(draftjs_content)
@@ -55,5 +61,29 @@ module SlickrHelper
                         :admin_image_index_path,
                         inst.additional_page_edit_paths
                       ].flatten)
+  end
+
+  def greeting_wording
+    now = Time.now
+    today = Date.today.to_time
+
+    early_morning = today.beginning_of_day
+    morning = today.beginning_of_day + 7.hours
+    noon = today.noon
+    evening = today.change( hour: 17 )
+    night = today.change( hour: 21 )
+    tomorrow = today.tomorrow
+
+    if (early_morning..morning).cover? now
+      'Early bird gets the worm'
+    elsif (morning..noon).cover? now
+      'Good Morning'
+    elsif (noon..evening).cover? now
+      'Good Afternoon'
+    elsif (evening..night).cover? now
+      'Good Evening'
+    elsif (night..tomorrow).cover? now
+      "Keep burning that midnight oil"
+    end
   end
 end
--- lib/slickr/version.rb
+++ lib/slickr/version.rb
@@ -1,3 +1,3 @@
 module Slickr
-  VERSION = '0.3.9'
+  VERSION = '0.3.10'
 end

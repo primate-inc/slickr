93e2be29f7f63eb0b5ad7b6fca582b25f4dddaa8
Jeff Knox <knoxjeffrey@outlook.com>
2018-03-01 12:26:08 +0000

    fix h5 and h6 icons

--- package/slickr/page_edit/text_editor_icons/h5.jsx
+++ package/slickr/page_edit/text_editor_icons/h5.jsx
@@ -3,7 +3,12 @@ import React from "react";
 export default class extends React.Component {
   render() {
     return (
-      <svg width="24" height="24" viewBox="0 0 24 24" ><path d="M11.66,19h-3V13H3.17v6h-3V5h3v5.48H8.7V5h3V19Z" fill="currentColor" fillRule="evenodd"/><path d="M23.79,16.1H22.11V19H19.23V16.1H13.46V14.05L19.4,5h2.71v8.78h1.68V16.1Zm-4.56-2.29V11.44c0-.08,0-0.2,0-0.36s0-.33,0-0.53,0-.4,0-0.61l0-.6c0-.19,0-0.36,0-0.5s0-.25,0-0.31H19.27q-0.17.4-.39,0.85a9.73,9.73,0,0,1-.49.9l-2.33,3.54h3.17Z" fill="currentColor" fillRule="evenodd"/></svg>
+      <svg width="20" height="20" viewBox="2 2 16 16" >
+        <path d="M9.9,14.7h-2v-4.1H4.1v4.1h-2V5.3h2V9h3.7V5.3h2V14.7z" fill="currentColor" fillRule="evenodd"/>
+        <path d="M14.9,8.7c0.9,0,1.6,0.3,2.2,0.8c0.5,0.5,0.8,1.2,0.8,2.1c0,1.1-0.3,1.9-1,2.4c-0.7,0.6-1.6,0.9-2.8,0.9
+  			c-1.1,0-1.9-0.2-2.5-0.5v-1.7c0.3,0.2,0.7,0.3,1.2,0.4c0.5,0.1,0.9,0.2,1.3,0.2c1.2,0,1.8-0.5,1.8-1.5c0-1-0.6-1.4-1.9-1.4
+  			c-0.2,0-0.5,0-0.8,0.1c-0.3,0-0.5,0.1-0.7,0.1l-0.8-0.4l0.4-4.8h5.1V7h-3.4l-0.2,1.9l0.2,0C14.1,8.7,14.5,8.7,14.9,8.7z" fill="currentColor" fillRule="evenodd"/>
+      </svg>
     );
   }
 }

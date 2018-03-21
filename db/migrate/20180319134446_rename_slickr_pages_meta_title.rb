114ffbd14ca9bad7ae8f6f3375ae8124683628e3
Bart Oleszczyk <bart@primate.co.uk>
2018-03-21 13:39:29 +0000

    Page meta title update

--- db/migrate/20180319134446_rename_slickr_pages_meta_title.rb
+++ db/migrate/20180319134446_rename_slickr_pages_meta_title.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Renaming page meta title column
 class RenameSlickrPagesMetaTitle < ActiveRecord::Migration[5.1]
   def change
     rename_column :slickr_pages, :meta_title, :page_title

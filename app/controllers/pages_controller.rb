45363fa3f479af86777a8e4e3d74c0a25369405b
Bart Oleszczyk <bart@primate.co.uk>
2018-03-22 12:19:18 +0000

    Page title as string variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   def show
     slug =  params[:slug].class == Symbol ? params[:slug] : params[:slug].split('/').last
     @slickr_page = Slickr::Page.where(slug: slug, aasm_state: :published).first
-    @slickr_meta_additional = { page_title: @slickr_page.page_title }
+    @slickr_page_title = @slickr_page.page_title
     raise AbstractController::ActionNotFound.new unless @slickr_page
     render layout: false
   end
--- app/helpers/slickr_helper.rb
+++ app/helpers/slickr_helper.rb
@@ -6,7 +6,7 @@ module SlickrHelper
 
   def slickr_page_title
     [
-      @slickr_meta_additional.try(:[], :page_title),
+      @slickr_page_title,
       @slickr_settings.try(:[], :site_title)
     ].reject(&:blank?).join(' - ')
   end

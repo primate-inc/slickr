b703b0c42f18870b87e2ce1a991fdebe50c561ac
Jeff Knox <knoxjeffrey@outlook.com>
2018-03-29 09:47:11 +0100

    change ref to slickr for videos

--- app/models/draftjs_exporter/generators/vimeo_generator.rb
+++ app/models/draftjs_exporter/generators/vimeo_generator.rb
@@ -2,7 +2,7 @@ require "erb"
 
 class DraftjsExporter::Generators::VimeoGenerator
   def initialize contents
-    slickr = Bundler.rubygems.find_name('slickr').first.full_gem_path
+    slickr = Bundler.rubygems.find_name('slickr_cms').first.full_gem_path
     @contents = contents
     @template = File.read "#{slickr}/app/views/vimeo/iframe.html.erb"
   end

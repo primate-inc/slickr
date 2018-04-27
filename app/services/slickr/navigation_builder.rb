dd8360e034ba7d8a167b6855f921137a7ae48882
Jeff Knox <knoxjeffrey@outlook.com>
2018-04-27 16:24:20 +0100

    tidied up and commented NavigationBuilder service

--- app/services/slickr/navigation_builder.rb
+++ app/services/slickr/navigation_builder.rb
@@ -1,7 +1,25 @@
-module Slickr
-  class NavigationBuilder
-    def initialize; end
+# frozen_string_literal: true
 
+module Slickr
+  # NavigationBuilder class
+  class NavigationBuilder
+    # example result
+    # {
+    #   :pathnames=>
+    #     [
+    #       {:page_id=>1, :path=>"/level1"},
+    #       {:page_id=>2, :path=>"/level1/level2"}
+    #     ],
+    #   :nav_menus=>
+    #     {
+    #       "Main navigation"=>
+    #         [{
+    #           :title=>"Menu", :image=>nil, :text=> {
+    #             :text=>"some text", :page_header=>nil, :page_into=>nil
+    #           }, :link=>"/menu", :link_text=>"Menu", "children"=>[]
+    #         }]
+    #     }
+    # }
     def nav_helper
       nav_trees = Slickr::Navigation.all_nav_trees
       pathnames = all_pages_pathnames(nav_trees)
@@ -11,6 +29,12 @@ module Slickr
       }
     end
 
+    private
+
+    #####################
+    # Build pathnames
+    #####################
+
     # example result
     # [{:page_id=>1, :path=>"/level1"}, {:page_id=>2, :path=>"/level1/level2"}]
     def all_pages_pathnames(nav_trees)
@@ -19,94 +43,6 @@ module Slickr
         nav_trees, main_page_paths
       )
       main_page_paths + additonal_page_paths
-    end
-
-    def all_nav_menus(nav_trees, pathnames)
-      menu_hash = {}
-      nav_trees.map do |root|
-        menu_hash[root['title']] = root['children'].map do |child_hash|
-          if root['root_type'] == 'Page'
-            parent_link = pathnames.select do |path|
-              path[:page_id] == root['page_id']
-            end[0][:path]
-          else
-            parent_link = '/'
-          end
-          build_nav(child_hash, pathnames, {}, parent_link)
-        end
-      end
-      menu_hash
-    end
-
-    def build_nav(child_hash, pathnames, hash, parent_link)
-      if child_hash['child_type'] == 'Page'
-        parent_link = pathnames.select do |path|
-          path[:page_id] == child_hash['page_id']
-        end[0][:path]
-      else
-        parent_link = parent_link
-      end
-      menu_hash = case child_hash['child_type']
-      when 'Page'
-        build_page_nav(child_hash, pathnames)
-      when 'Header'
-        build_header_nav(child_hash, pathnames)
-      when 'Custom Link'
-        build_custom_link_nav(child_hash, pathnames)
-      when 'Anchor'
-        build_anchor_link_nav(child_hash, pathnames, parent_link)
-      end
-      menu_hash['children'] = child_hash['children'].map do |child_hash|
-        build_nav(child_hash, pathnames, menu_hash, parent_link)
-      end
-      menu_hash
-    end
-
-    def build_page_nav(hash, pathnames)
-      {
-        title: hash['title'],
-        image: hash['image'] ? hash['image'] : hash['page_header_image'],
-        text: {
-          text: hash['text'], page_header: hash['page_header'],
-          page_into: hash['page_into']
-        },
-        link: pathnames.select do |path|
-          path[:page_id] == hash['page_id']
-        end[0][:path],
-        link_text: hash['link_text'] ? hash['link_tex'] : hash['title']
-      }
-    end
-
-    def build_header_nav(hash, pathnames)
-      {
-        title: hash['title'],
-        image: hash['image'],
-        text: hash['text']
-      }
-    end
-
-    def build_custom_link_nav(hash, pathnames)
-      {
-        title: hash['title'],
-        image: hash['image'],
-        text: hash['text'],
-        link: hash['link'],
-        link_text: hash['link_text']
-      }
-    end
-
-    def build_anchor_link_nav(hash, pathnames, parent_link)
-      {
-        title: hash['title'],
-        image: hash['image'],
-        text: hash['text'],
-        link: parent_link + hash['link'],
-        link_text: hash['link_text']
-      }
-    end
-
-    def all_nav_trees
-      first.build_tree_structure[0]['children']
     end
 
     # build up all of the pathnames nested under a root_type of 'Root'
@@ -129,10 +65,8 @@ module Slickr
     )
       navs = nav_trees.map { |root| root if root['root_type'] == 'Page' }
       navs.compact.map do |hash|
-        start_path_name = main_page_paths.select do |path|
-          path[:page_id] == hash['page_id']
-        end
-        iterate_children_for_pathnames(hash, "#{start_path_name[0][:path]}/")
+        start_path = path_finder(main_page_paths, hash)[:path]
+        iterate_children_for_pathnames(hash, "#{start_path}/")
       end.flatten
     end
 
@@ -159,6 +93,108 @@ module Slickr
         end
       end
       array
+    end
+
+    #####################
+    # Build nav menus
+    #####################
+
+    # example result
+    # {
+    #   "Main navigation"=>
+    #     [{
+    #       :title=>"Menu", :image=>nil, :text=> {
+    #         :text=>"some text", :page_header=>nil, :page_into=>nil
+    #       }, :link=>"/menu", :link_text=>"Menu", "children"=>[]
+    #     }],
+    #   "Header"=>
+    #     [{
+    #       :title=>"Menu", :image=>nil
+    #     }]
+    # }
+    def all_nav_menus(nav_trees, pathnames)
+      menu_hash = {}
+      nav_trees.map do |root|
+        menu_hash[root['title']] = root['children'].map do |child_hash|
+          parent_link = if root['root_type'] == 'Page'
+                          path_finder(pathnames, root)[:path]
+                        else
+                          '/'
+                        end
+          build_nav(child_hash, pathnames, parent_link)
+        end
+      end
+      menu_hash
+    end
+
+    # Builds the hash for an individual nav menu.
+    # example result
+    # {
+    #   :title=>"Menu", :image=>nil, :text=> {
+    #     :text=>"some text", :page_header=>nil, :page_into=>nil
+    #   }, :link=>"/menu", :link_text=>"Menu", "children"=>[]
+    # }
+    def build_nav(child_hash, pathnames, parent_link)
+      parent_link = if child_hash['child_type'] == 'Page'
+                      path_finder(pathnames, child_hash)[:path]
+                    else
+                      parent_link
+                    end
+      menu_hash = case child_hash['child_type']
+                  when 'Page'
+                    build_page_nav(child_hash, pathnames)
+                  when 'Header'
+                    build_header_nav(child_hash)
+                  when 'Custom Link'
+                    build_custom_link_nav(child_hash)
+                  when 'Anchor'
+                    build_anchor_link_nav(child_hash, parent_link)
+                  end
+      menu_hash['children'] = child_hash['children'].map do |hash|
+        build_nav(hash, pathnames, parent_link)
+      end
+      menu_hash
+    end
+
+    def build_page_nav(hash, pathnames)
+      {
+        title: hash['title'],
+        image: hash['image'] ? hash['image'] : hash['page_header_image'],
+        text: {
+          text: hash['text'], page_header: hash['page_header'],
+          page_into: hash['page_into']
+        },
+        link: path_finder(pathnames, hash)[:path],
+        link_text: hash['link_text'] ? hash['link_tex'] : hash['title']
+      }
+    end
+
+    def build_header_nav(hash)
+      {
+        title: hash['title'], image: hash['image'], text: hash['text']
+      }
+    end
+
+    def build_custom_link_nav(hash)
+      {
+        title: hash['title'], image: hash['image'], text: hash['text'],
+        link: hash['link'], link_text: hash['link_text']
+      }
+    end
+
+    def build_anchor_link_nav(hash, parent_link)
+      {
+        title: hash['title'], image: hash['image'], text: hash['text'],
+        link: parent_link + hash['link'], link_text: hash['link_text']
+      }
+    end
+
+    # example result
+    # {:page_id=>1, :path=>"/page-1"}
+    def path_finder(pathnames, hash)
+      pathnames.select do |path|
+        path[:page_id] == hash['page_id']
+      end[0]
     end
   end
 end

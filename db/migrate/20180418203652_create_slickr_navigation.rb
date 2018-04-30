6672ade8ba2216d70d1957c9a82625ad7b0fdeb5
Jeff Knox <knoxjeffrey@outlook.com>
2018-04-30 16:45:03 +0100

    update migration for nav

--- db/migrate/20180418203652_create_slickr_navigation.rb
+++ db/migrate/20180418203652_create_slickr_navigation.rb
@@ -2,12 +2,12 @@ class CreateSlickrNavigations < ActiveRecord::Migration[5.1]
   def change
     create_table :slickr_navigations do |t|
       t.string      :ancestry
-      t.integer     :slickr_page_id
+      t.references  :slickr_page
+      t.references  :slickr_image
       t.integer     :position
       t.string      :root_type
       t.string      :child_type
       t.string      :title
-      t.string      :image
       t.text        :text
       t.string      :link
       t.string      :link_text

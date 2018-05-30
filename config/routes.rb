a6695fcb2a6db5ce48061e1c38fd85c6f7f0ad95
Jeff Knox <knoxjeffrey@outlook.com>
2018-05-31 00:18:43 +0100

    remove old page route helper in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,31 +5,11 @@ class MultiContraint
   end
 
   def matches?(request)
-    # matches = true
-    # @constraints.each do |constraint|
-    #   matches &&= constraint.matches?(request)
-    # end
-    # matches
-
-    # temp until all sites moved onto new nav
-    count = 0
+    matches = true
     @constraints.each do |constraint|
-      count += 1 if constraint.matches?(request)
+      matches &&= constraint.matches?(request)
     end
-    true if count > 0
-  end
-end
-
-module SlickrPageRouteHelper
-  def self.matches?(request)
-    request = request.path
-    slug_sections = request.split('/').reject(&:empty?)
-    matching_page = Slickr::Page.where(
-      aasm_state: :published, slug: slug_sections.last
-    )[0]
-    return false if matching_page.nil?
-    tree_sections = matching_page.root? ? [matching_page.slug] : matching_page.self_and_ancestors.pluck(:slug).reverse - [matching_page.root.slug]
-    return slug_sections == tree_sections
+    matches
   end
 end
 
@@ -47,7 +27,7 @@ Rails.application.routes.draw do
   %w[404 500].each do |code|
     get code, to: 'errors#show', code: code
   end
-  constraints(MultiContraint.new([SlickrPageRouteHelper, SlickrNavRouteHelper])) do
+  constraints(MultiContraint.new([SlickrNavRouteHelper])) do
     get '*slug' => 'pages#show', as: 'show_page'
   end
 

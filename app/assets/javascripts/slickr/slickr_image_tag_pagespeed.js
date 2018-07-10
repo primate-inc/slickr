88aa3181e8a9f7ea554318711f703030c5396fd7
Jeff Knox <knoxjeffrey@outlook.com>
2018-07-10 23:26:06 +0100

    try adding pagespeed no transform

--- app/assets/javascripts/slickr/slickr_image_tag_pagespeed.js
+++ app/assets/javascripts/slickr/slickr_image_tag_pagespeed.js
@@ -1,12 +1,21 @@
 $(function () {
+  $('.slickr_image_tag').each(function(i, obj) {
+    var image = obj.getElementsByTagName('img')[0];
 
-  // $('.slickr_image_tag').each(function(i, obj) {
-  //   var currentSrc = obj.getElementsByTagName('img')[0].currentSrc;
-  //   var sourceTags = obj.getElementsByTagName('source');
-  //   console.log(sourceTags)
-  //
-  //   // sourceTags.each(function(i, obj) {
-  //   //   console.log(obj.srcset)
-  //   // })
-  // });
+    if (image.hasAttribute('data-pagespeed-lazy-src')) {
+      var minAspectRatio = image.dataset.min_aspect_ratio;
+      var width = image.offsetWidth;
+      image.style.minHeight = (width * minAspectRatio) + 'px';
+    }
+  });
+
+  window.addEventListener('resize', function(i, obj) {
+    $('.slickr_image_tag').each(function(i, obj) {
+      var image = obj.getElementsByTagName('img')[0];
+
+      if (!image.hasAttribute('data-pagespeed-lazy-src')) {
+        image.style.minHeight = null
+      }
+    });
+  });
 })

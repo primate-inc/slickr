0b48bf0a2a4ada3197ee38a22f260cf9ae965aed
Jeff Knox <knoxjeffrey@outlook.com>
2018-07-11 01:25:18 +0100

    too many issues with pagespeed and picture tag so just allow preload and no longer lazyload

--- app/helpers/slickr_image_helper.rb
+++ app/helpers/slickr_image_helper.rb
@@ -33,29 +33,15 @@ module SlickrImageHelper
 
   def build_sources(image, args)
     args[:slickr].map do |info|
-      meta_data = image.image_data[info[:size].to_s]['metadata']
-      aspect_ratio = (meta_data['height'].to_f / meta_data['width']).round(2)
       "<source media='(#{info.keys[0]}: #{info.values[0]})'
-       data-srcset='#{image.image_url(info[:size])}'
-       data-aspect_ratio='#{aspect_ratio}'/>"
+       data-srcset='#{image.image_url(info[:size])}' />"
     end
   end
 
   def build_image_tag(image, args)
     image_tag_options = args.except(:slickr)
-    image_tag_options['data-min_aspect_ratio'] = min_aspect_ratio(image, args)
-    image_tag_options[:style] = 'background-color:white;'
     image_tag image.image_url(args[:slickr][0][:size]),
               image_tag_options
   end
 
-  def min_aspect_ratio(image, args)
-    aspect_ratio = 1
-    args[:slickr].map do |info|
-      meta_data = image.image_data[info[:size].to_s]['metadata']
-      ar = (meta_data['height'].to_f / meta_data['width']).round(2)
-      aspect_ratio = ar if ar < aspect_ratio
-    end
-    aspect_ratio
-  end
 end

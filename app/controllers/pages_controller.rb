d31b13c92e0bac8c5ebb05e8c079ceba5643f539
Jeff Knox <knoxjeffrey@outlook.com>
2019-01-15 23:04:51 +0000

    fix pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   def show
     slug =  params[:slug].class == Symbol ? params[:slug] : params[:slug].split('/').last
     @slickr_page = Slickr::Page.left_outer_joins(:schedule)
-                               .select('*', 'slickr_schedules.id AS schedule_id')
+                               .select('*', :id, 'slickr_schedules.id AS schedule_id')
                                .find_by(
                                  'slug = ? AND slickr_schedules.id IS NULL',
                                  slug

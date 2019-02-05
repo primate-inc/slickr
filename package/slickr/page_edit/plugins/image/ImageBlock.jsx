f4783d8f865eef9351eec79d20a4f3a4d4eb6047
lost-in-code-au <reece.c.jones@gmail.com>
2019-02-05 12:14:29 +0000

    update to standard image to have more additional_info

--- package/slickr/page_edit/plugins/image/ImageBlock.jsx
+++ package/slickr/page_edit/plugins/image/ImageBlock.jsx
@@ -10,6 +10,8 @@ export default class ImageBlock extends React.Component {
     super(props);
 
     this.handleCaptionChange = this.handleCaptionChange.bind(this);
+    this.handleAltTextChange = this.handleAltTextChange.bind(this);
+    this.handleCreditChange = this.handleCreditChange.bind(this);
 
     this.actions = [{
       "key": "delete",
@@ -20,11 +22,66 @@ export default class ImageBlock extends React.Component {
 
   handleCaptionChange(e) {
     e.stopPropagation();
-    this.props.container.updateData({caption: e.target.value});
+    const image = this.props.data.image
+    image.additional_info.img_title = e.target.value
+    this.props.container.updateData({ image: image });
   }
 
+  handleAltTextChange(e) {
+    e.stopPropagation();
+    const image = this.props.data.image
+    image.additional_info.alt_text = e.target.value
+    this.props.container.updateData({ image: image });
+  }
+
+  handleCreditChange(e) {
+    e.stopPropagation();
+    const image = this.props.data.image
+    image.additional_info.img_credit = e.target.value
+    this.props.container.updateData({ image: image });
+  }
+
+  _submitAdditionalInfo = () => {
+    // id: values.id,
+    // additional_info: {
+    //   alt_text: values.alt_text,
+    //   img_title: values.img_title,
+    //   img_credit: values.img_credit
+    // }
+  }
+
+
+  // _loadDownloads = (downloadString, textAreaStoreIndex) => {
+  //   let params = {};
+  //   params[_csrf_param()] = _csrf_token()
+  //   params["download_string"] = downloadString;
+
+  //   request.get(text_editor_store[textAreaStoreIndex]
+  //     .getState().pageState.admin_search_downloads_path)
+  //     .set('Accept', 'json').query(params).end(function(err,resp){
+  //     if(err) {
+  //       console.error(err)
+  //     } else {
+  //       text_editor_store[textAreaStoreIndex].dispatch({
+  //         type: 'LOAD_DOWNLOADS',
+  //         payload: resp.body
+  //       })
+  //     }
+  //   })
+
+  //   this.props.container.onChange('load_downloads')
+
+  //   this.loadData(textAreaStoreIndex).then(() => {
+  //     let downloads = text_editor_store[textAreaStoreIndex].getState().loadedDownloads
+  //     this.setState({
+  //       loadedDownloads: downloads,
+  //       loading: false
+  //     });
+  //   });
+  // }
+
+
   render(){
-    // console.log('image', this.props.data.image);
     return (
       <CommonBlock {...this.props} actions={this.actions}>
         <BlockContent>
@@ -35,9 +92,9 @@ export default class ImageBlock extends React.Component {
         </BlockContent>
         <BlockData>
           <BlockInput
-            placeholder="Atl Tag"
+            placeholder="Atl text"
             value={this.props.data.image.additional_info.alt_text}
-            onChange={this.handleAltTagChange} />
+            onChange={this.handleAltTextChange} />
           <BlockInput
             placeholder="Caption"
             value={this.props.data.image.additional_info.img_title}
@@ -45,7 +102,7 @@ export default class ImageBlock extends React.Component {
           <BlockInput
             placeholder="Credit"
             value={this.props.data.image.additional_info.img_credit}
-            onChange={this.handleCaptionChange} />
+            onChange={this.handleCreditChange} />
         </BlockData>
       </CommonBlock>
     );

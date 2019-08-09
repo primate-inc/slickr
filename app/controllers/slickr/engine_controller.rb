30ba4f5d67c29e031edd5d64bd622e29ad5d1654
lost-in-Code-au <reece@primate.co.uk>
2019-08-09 11:54:08 +0100

    mend

--- app/controllers/slickr/engine_controller.rb
+++ app/controllers/slickr/engine_controller.rb
@@ -12,9 +12,9 @@ module Slickr
     def fetch_slickr_settings
       meta_defaults = {og_type: 'article'}
       @slickr_settings = meta_defaults.merge(Slickr::Setting.get_all)
-      is_preview = params[:action] == "preview"
+      @is_preview = params[:action] == "preview"
 
-      @slickr_nav_helper = Slickr::NavigationBuilder.new.nav_helper(is_preview)
+      @slickr_nav_helper = Slickr::NavigationBuilder.new.nav_helper
     end
   end
 end
--- lib/slickr/version.rb
+++ lib/slickr/version.rb
@@ -1,3 +1,3 @@
 module Slickr
-  VERSION = '0.10.6.21'
+  VERSION = '0.10.6.22'
 end

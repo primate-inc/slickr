17168f9306ab74d3df76d373893051549161991a
lost-in-Code-au <reece@primate.co.uk>
2019-08-09 10:26:17 +0100

    update for preview

--- app/controllers/slickr/engine_controller.rb
+++ app/controllers/slickr/engine_controller.rb
@@ -1,9 +1,20 @@
+# module Slickr
+#   module EngineController
+#     def fetch_slickr_settings
+#       meta_defaults = {og_type: 'article'}
+#       @slickr_settings = meta_defaults.merge(Slickr::Setting.get_all)
+#       @slickr_nav_helper = Slickr::NavigationBuilder.new.nav_helper
+#     end
+#   end
+# end
 module Slickr
   module EngineController
     def fetch_slickr_settings
       meta_defaults = {og_type: 'article'}
       @slickr_settings = meta_defaults.merge(Slickr::Setting.get_all)
-      @slickr_nav_helper = Slickr::NavigationBuilder.new.nav_helper
+      is_preview = params[:action] == "preview"
+
+      @slickr_nav_helper = Slickr::NavigationBuilder.new.nav_helper(is_preview)
     end
   end
 end

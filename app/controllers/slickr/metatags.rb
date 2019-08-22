602ba6549ad4a0007d55a5af4ff742e599b5bb24
Bart O <bart@primate.co.uk>
2019-08-22 12:06:34 +0100

    added asset host for og and twitter images

--- app/controllers/slickr/metatags.rb
+++ app/controllers/slickr/metatags.rb
@@ -12,11 +12,11 @@ module Slickr
         meta_description: meta_tags.try(:meta_description),
         og_title: meta_tags.try(:og_title),
         og_description: meta_tags.try(:og_description),
-        og_image: inst_var.try(:og_image).try(:image_url, :l_limit),
+        og_image: inst_var.try(:og_image).try(:image_url, :l_limit, host: ActionController::Base.asset_host),
         og_url: request.original_url,
         twitter_title: meta_tags.try(:twitter_title),
         twitter_description: meta_tags.try(:twitter_description),
-        twitter_image: inst_var.try(:twitter_image).try(:image_url, :l_limit)
+        twitter_image: inst_var.try(:twitter_image).try(:image_url, :l_limit, host: ActionController::Base.asset_host)
       }
     end
   end
--- lib/slickr/version.rb
+++ lib/slickr/version.rb
@@ -1,3 +1,3 @@
 module Slickr
-  VERSION = '0.15.9.4'
+  VERSION = '0.15.9.5'
 end

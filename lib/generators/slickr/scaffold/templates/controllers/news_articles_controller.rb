551c67b942c606cf37d1c701bffd36d026d8344a
Bart O <bart@primate.co.uk>
2019-08-29 00:55:26 +0100

    added news articles generator

---
+++ lib/generators/slickr/scaffold/templates/controllers/news_articles_controller.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# Scaffold News Articles Controller
+class NewsArticlesController < ApplicationController
+  protect_from_forgery except: :index
+  before_action :assign_meta_data, only: %i[index]
+
+  def index
+    @filters = initialize_filterrific(NewsArticle, filter_params) || return
+    @news = @filters.find.is_published.order(date: :desc).page(params[:page])
+
+    respond_to do |format|
+      format.html do
+        render :index,
+               layout: '../slickr_page_templates/news',
+               locals: { articles: @news }
+      end
+      # format.json { render json: @news.to_json }
+      format.js { render :index, layout: false }
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    # There is an issue with the persisted param_set. Reset it.
+    puts "Had to reset filterrific params: #{e.message}"
+    redirect_to(reset_filterrific_url(format: :html)) && return
+  end
+
+  def show
+    @news = NewsArticle.is_published.friendly.find(params[:id])
+    insert_slickr_meta_tags(@news)
+  end
+
+  private
+
+  def filter_params
+    params[:filterrific].present? ? params[:filterrific] : { has_category: [] }
+  end
+
+  def assign_meta_data
+    @page = Slickr::Page.find_by(layout: params[:controller]) ||
+            Slickr::Page.new
+    @slickr_page_title = @page['title']
+    @slickr_meta_override = generate_meta_data(@page, @slickr_page_title)
+  end
+
+  def generate_meta_data(item, title)
+    {
+      meta_description: item.meta_description,
+      og_title: title,
+      og_description: item.meta_description,
+      twitter_description: item.meta_description,
+      og_url: request.original_url,
+      twitter_title: title
+    }
+  end
+end
---
+++ lib/generators/slickr/scaffold/templates/test/models/news_articles_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class NewsArtcileTest < ActiveSupport::TestCase
+  context "relations" do
+    should have_one(:news_header_image)
+    should have_one(:news_thumbnail)
+  end
+
+  context "validations" do
+    should validate_presence_of(:title)
+    should validate_presence_of(:category)
+    should validate_inclusion_of(:category).in_array(::NewsArticle::CATEGORIES.map(&:to_s))
+  end
+
+  context "database" do
+    should have_db_column(:slug).of_type(:string)
+    should have_db_column(:title).of_type(:string)
+    should have_db_column(:header).of_type(:text)
+    should have_db_column(:featured).of_type(:boolean).with_options(null: false, default: false)
+    should have_db_column(:category).of_type(:string)
+    should have_db_column(:header_image).of_type(:string)
+    should have_db_column(:thumbnail).of_type(:string)
+    should have_db_column(:content).of_type(:text)
+    should have_db_column(:date).of_type(:date)
+
+    should have_db_column(:created_at).of_type(:datetime)
+    should have_db_column(:updated_at).of_type(:datetime)
+
+    should have_db_index(:slug)
+  end
+
+end

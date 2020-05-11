6058a890e97f31bc3e4c3d1c3b5790e69b95afd4
Bart O <bart@primate.co.uk>
2020-05-11 21:25:46 +0100

    removed trackable migreation from admin setup

--- lib/generators/slickr/templates/migrations/add_trackable_to_devise.rb
+++
@@ -1,9 +0,0 @@
-class AddTrackableToDevise < ActiveRecord::Migration[5.1]
-  def change
-    add_column :admin_users, :sign_in_count, :integer, default: 0, null: false
-    add_column :admin_users, :current_sign_in_at, :datetime
-    add_column :admin_users, :last_sign_in_at, :datetime
-    add_column :admin_users, :current_sign_in_ip, :inet
-    add_column :admin_users, :last_sign_in_ip, :inet
-  end
-end

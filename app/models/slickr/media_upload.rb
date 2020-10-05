fcf51eebfb033718c90a9acf59cf8fa7c23e374d
Bart O <bart@primate.co.uk>
2020-10-05 14:05:34 +0100

    updated resizing

--- app/models/slickr/media_upload.rb
+++ app/models/slickr/media_upload.rb
@@ -124,7 +124,7 @@ module Slickr
         transitions from: :uploaded, to: :processing
       end
       event :resize, after: :send_for_resizing do
-        transitions from: :processing, to: :resizing
+        transitions from: :processing, to: :resizing, unless: :resizing?
       end
       event :finalize do
         transitions from: :resizing, to: :ready
--- lib/slickr/version.rb
+++ lib/slickr/version.rb
@@ -1,3 +1,3 @@
 module Slickr
-  VERSION = '0.20.4.3'
+  VERSION = '0.20.4.4'
 end

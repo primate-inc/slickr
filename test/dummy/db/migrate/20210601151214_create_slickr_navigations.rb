4c7e601ac2aea3afac976d5a0d89c7528070c1e3
Bart O <bart@primate.co.uk>
2021-06-09 13:25:55 +0100

    extracted previewable to separate gem

---
+++ test/dummy/db/migrate/20210601151214_create_slickr_navigations.rb
@@ -0,0 +1,20 @@
+class CreateSlickrNavigations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :slickr_navigations do |t|
+      t.string      :ancestry
+      t.references  :slickr_page
+      t.integer     :position
+      t.string      :root_type
+      t.string      :child_type
+      t.string      :title
+      t.text        :text
+      t.string      :link
+      t.string      :link_text
+      t.string      :alt_link_text
+      t.string      :config_string
+
+      t.timestamps
+    end
+    add_index :slickr_navigations, :ancestry
+  end
+end

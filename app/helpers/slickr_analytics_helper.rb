8795d33cf3955a79a65a63eaf9d1aeaf6d409b95
Bart O <bart@primate.co.uk>
2021-10-07 23:45:02 +0100

    gtm update

--- app/helpers/slickr_analytics_helper.rb
+++ app/helpers/slickr_analytics_helper.rb
@@ -19,23 +19,20 @@ module SlickrAnalyticsHelper
   def google_tag_manager_script(identifier)
     identifier = sanitize_id(identifier)
     <<-HTML.html_safe
-      <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script>
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','#{identifier}');</script>
-      <!-- End Google Tag Manager -->
+      })(window,document,'script','dataLayer','#{identifier}');
+    </script>
     HTML
   end
 
   def google_tag_manager_noscript(identifier)
     identifier = sanitize_id(identifier)
     <<-HTML.html_safe
-      <!-- Google Tag Manager (noscript) -->
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=#{identifier}"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      <!-- End Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=#{identifier}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     HTML
   end
 
